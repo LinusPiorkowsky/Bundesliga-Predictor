162fe7dac90a3ae014f5c9d8d57ba0b849e95e38
LinusPiorkowsky <s_piorkowsky23@stud.hwr-berlin.de>
2024-11-01 12:21:16 +0100

    DB setup tests

---
+++ sql/create_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Games (
+    game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    match_date TEXT,
+    season INTEGER,
+    home_team_id INTEGER,
+    away_team_id INTEGER,
+    goals_home INTEGER,
+    goals_away INTEGER,
+    draw BOOLEAN,
+    win_home BOOLEAN,
+    win_away BOOLEAN,
+    complete BOOLEAN DEFAULT FALSE,
+    description TEXT NOT NULL,
+    FOREIGN KEY (home_team_id) REFERENCES Teams(team_id),
+    FOREIGN KEY (away_team_id) REFERENCES Teams(team_id)
+);
+
+CREATE TABLE Teams (
+    team_id INTEGER PRIMARY KEY,
+    team_name TEXT UNIQUE NOT NULL,
+    description TEXT
+);
+
+
+
